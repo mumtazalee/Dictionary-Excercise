db290115dc1336af850a1754f8e2b2b349f91205
mumtazalee <125261688+mumtazalee@users.noreply.github.com>
2024-11-15 15:28:50 +0000

    Create Intermidate.py

---
+++ Intermidate.py
@@ -0,0 +1,31 @@
+car ={
+    'make':'Toyota',
+    'model':'Yaris',
+    'year':'2021',
+    'specifications': {
+        'exterior features':'360 camera',
+        'interior features':'dash camera',
+        'performance': 'top speed'
+    },
+    
+    'questionare': {
+        'car mileage':'45000',
+        'car colour':'white',
+        'car MOT':'Till Nov 2025',
+    
+    }
+}
+
+
+
+car['make']
+car['model']
+car['year']
+car['specifications']['exterior features']
+car['specifications']['interior features']
+car['specifications']['performance']
+car['questionare']['car mileage']
+car['questionare']['car colour']
+car['questionare']['car MOT']
+print (car)
+
